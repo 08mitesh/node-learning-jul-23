0b7c6aae311efaaf23f8e6dea7eb41414f264511
Mitesh <miteshbhanushali08@gmail.com>
2023-07-23 14:18:25 +0530

    removed api key

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require("request")
 
-const url = "http://api.weatherstack.com/current?access_key=1da43c2e1c5495d4d4eabcb424194b75&query=Mumbai"
+const url = "http://api.weatherstack.com/current?access_key={API_KEY}&query=Mumbai"
 
 request({uri:url},(e,res) => {
     console.log(res);
